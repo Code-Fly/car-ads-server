e9d6af22ebf07e36e094f1936ca56477ed0c02c8
Barrie <barrieshieh@gmail.com>
2018-05-06 02:34:19 +0800

    update

--- management-ui/src/main/java/com/cloud/carads/management/controller/LoginController.java
+++ management-ui/src/main/java/com/cloud/carads/management/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.cloud.carads.management.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/")
 public class LoginController {
+    @Value("${wechat.setting.appid}")
+    public String WECHAT_SETTING_APP_ID;
+
+    @Value("${wechat.setting.appsecret}")
+    public String WECHAT_SETTING_APP_SECRET;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model) {
         model.addAttribute("name", "Dear");
+        model.addAttribute("appId", WECHAT_SETTING_APP_ID);
+        model.addAttribute("appSecret", WECHAT_SETTING_APP_SECRET);
+
         return "login";
     }
 
