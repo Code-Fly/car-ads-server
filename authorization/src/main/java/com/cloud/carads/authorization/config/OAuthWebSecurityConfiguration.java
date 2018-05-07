b122ac28dd88ab641cd1a6ad26747fa3681d8125
Barrie <barrieshieh@gmail.com>
2018-05-08 00:58:02 +0800

    add oauth2 server

--- authorization/src/main/java/com/cloud/carads/authorization/config/OAuthWebSecurityConfiguration.java
+++ authorization/src/main/java/com/cloud/carads/authorization/config/OAuthWebSecurityConfiguration.java
@@ -1,44 +1,61 @@
 package com.cloud.carads.authorization.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 
 @Configuration
 public class OAuthWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        super.configure(http);
-        http.cors()
-                .and()
-                .csrf().disable()
-                .antMatcher("/**") // 捕捉所有路由
+        http
+                .cors()
+                    .and()
+                .csrf()
+                    .disable()
+                    .antMatcher("/**") // 捕捉所有路由
                 .authorizeRequests()
-                .antMatchers("/oauth/**").permitAll()
-                .antMatchers("/assets/**").permitAll()
-//                .antMatchers("/oauth/logout").permitAll()
-//                .antMatchers("/oauth/exit").permitAll()
-
-
-                .anyRequest().authenticated()
-                .and()
+                    .antMatchers("/oauth/**").permitAll()
+                    .antMatchers("/assets/**").permitAll()
+    //                .antMatchers("/oauth/logout").permitAll()
+    //                .antMatchers("/oauth/exit").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
                 .exceptionHandling()
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/oauth/login"))
-                .and().formLogin()
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .loginPage("/oauth/login")
-                .failureUrl("/oauth/login?error=1")
-                .permitAll()
+                    .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/oauth/login"))
+                    .and()
+                .formLogin()
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .loginPage("/oauth/login")
+                    .failureUrl("/oauth/login?error=1")
+                    .and()
+//                .sessionManagement()
+//                    .sessionAuthenticationErrorUrl("/oauth/login?error=2")
+//                    .invalidSessionUrl("/oauth/login?error=3")
+//                    .maximumSessions(1)
+//                    .expiredUrl("/oauth/login?error=4")
+//                    .maxSessionsPreventsLogin(false)
+//                    .sessionRegistry(sessionRegistry())
+//                    .and()
+
 //                .and()
 //                .logout().logoutUrl("/oauth/logout")
 //                .logoutSuccessUrl("/oauth/exit")
 //                .permitAll()
-                .and().httpBasic();
+                .httpBasic();
     }
 
     @Override
