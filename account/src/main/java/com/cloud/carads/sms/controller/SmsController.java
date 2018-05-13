6e85d55bdf97f93e60a3c859548368cdf858d7fb
Partys88 <Zqw19880706>
2018-05-13 11:54:54 +0800

    区域接口

---
+++ account/src/main/java/com/cloud/carads/sms/controller/SmsController.java
@@ -0,0 +1,13 @@
+package com.cloud.carads.sms.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/sms")
+@Api(description = "短信验证码接口")
+public class SmsController {
+
+
+}
