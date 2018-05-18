8f3d2f0633b3cfbfb54aa4b727352e5523a4af82
Barrie <barrieshieh@gmail.com>
2018-05-19 05:22:24 +0800

    update config file

--- account/src/main/java/com/cloud/carads/account/config/RestApiConfig.java
+++ account/src/main/java/com/cloud/carads/account/config/RestApiConfig.java
@@ -1,13 +1,21 @@
 package com.cloud.carads.account.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 /**
  * Created by Administrator on 2016/10/13.
@@ -20,6 +28,42 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Configuration
 public class RestApiConfig {
+    @Value("${security.oauth2.client.client-id}")
+    public String CLIENT_ID;
+
+    @Value("${security.oauth2.client.client-secret}")
+    public String CLIENT_SECRET;
+
+    @Value("${security.oauth2.client.scope}")
+    public String SCOPE;
+
+    @Value("${security.oauth2.client.access-token-uri}")
+    public String ACCESS_TOKEN_URI;
+
+    @Value("${security.oauth2.client.user-authorization-uri}")
+    public String USER_AUTHORIZATION_URI;
+
+    @Bean
+    public SecurityConfiguration security() {
+        return new SecurityConfiguration(CLIENT_ID,
+                CLIENT_SECRET,
+                "realm", "OAUTH_REALM",
+                "apiKey", ApiKeyVehicle.HEADER, "api_key", "");
+    }
+
+
+//    @Bean
+//    public SecurityConfiguration security() {
+//
+//
+//        return SecurityConfigurationBuilder.builder()
+//                .clientId(CLIENT_ID)
+//                .clientSecret(CLIENT_SECRET)
+//                .scopeSeparator(" ")
+//                .useBasicAuthenticationWithAccessCodeGrant(true)
+//                .realm("OAUTH_REALM")
+//                .build();
+//    }
 
     @Bean
     public Docket configSpringfoxDocketForApiUser() {
@@ -29,7 +73,9 @@ public class RestApiConfig {
                 .forCodeGeneration(true)
                 .select()
                 .paths(PathSelectors.regex("/account/.*"))
-                .build();
+                .build()
+                .securitySchemes(Arrays.asList(securityScheme()))
+                .securityContexts(Arrays.asList(securityContext()));
     }
 
     @Bean
@@ -65,6 +111,7 @@ public class RestApiConfig {
                 .build();
     }
 
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("帐号管理")
@@ -74,6 +121,36 @@ public class RestApiConfig {
 //                .license("LICENSE")
 //                .licenseUrl("http://url-to-license.com")
                 .build();
+    }
+
+    private SecurityScheme securityScheme() {
+        GrantType grantType = new AuthorizationCodeGrantBuilder()
+                .tokenEndpoint(new TokenEndpoint(ACCESS_TOKEN_URI, "access_token"))
+                .tokenRequestEndpoint(
+                        new TokenRequestEndpoint(USER_AUTHORIZATION_URI, CLIENT_ID, CLIENT_SECRET))
+                .build();
+
+        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+                .grantTypes(Arrays.asList(grantType))
+                .scopes(Arrays.asList(scopes()))
+                .build();
+        return oauth;
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+                .forPaths(PathSelectors.regex("/account/.*"))
+                .build();
+    }
+
+
+    private AuthorizationScope[] scopes() {
+        AuthorizationScope[] scopes = {
+                new AuthorizationScope("read", "for read operations"),
+                new AuthorizationScope("write", "for write operations"),
+                new AuthorizationScope("app", "Access foo API")};
+        return scopes;
     }
 
 }
