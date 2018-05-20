f3a72b2b45abf9ecf374fc49909a308cd8db49d6
Barrie <barrieshieh@gmail.com>
2018-05-21 02:46:45 +0800

    update error code

--- account/src/main/java/com/cloud/carads/account/controller/AccountController.java
+++ account/src/main/java/com/cloud/carads/account/controller/AccountController.java
@@ -4,14 +4,10 @@
 package com.cloud.carads.account.controller;
 
 import com.cloud.carads.account.entity.CAccountInfo;
-import com.cloud.carads.account.entity.CAccountInfoDto;
 import com.cloud.carads.account.service.IAccountService;
-import com.cloud.carads.account.service.ISMSService;
 import com.cloud.carads.commons.controller.BaseController;
 import com.cloud.carads.commons.entity.Error;
 import com.cloud.carads.commons.entity.ErrorMsg;
-import com.cloud.carads.commons.utils.MD5Util;
-import com.cloud.carads.constant.SystemConstant;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -32,9 +28,6 @@ import java.util.List;
 public class AccountController extends BaseController {
     @Autowired
     private IAccountService accountService;
-
-    @Autowired
-    private ISMSService smsService;
 
     @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ApiOperation(value = "获取车主信息列表")
@@ -154,35 +147,5 @@ public class AccountController extends BaseController {
                            @RequestBody List<Long> ids
     ) {
         return new ErrorMsg(Error.SUCCESS.getValue(), Error.SUCCESS.getReasonPhrase(), accountService.delete(ids));
-    }
-
-
-    @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ApiOperation(value = "车主注册 返回车主的id")
-    public ErrorMsg register(@ApiParam(value = "车主的基本信息")
-                             @RequestBody(required = true) CAccountInfoDto accountInfo
-    ) {
-        // 校验验证码
-        ErrorMsg smsLog = smsService.queryLastSMSByPhone(accountInfo.getMobileNo());
-        //ToDO
-       /* if (!accountInfo.getShortCode().equals(smsLog.getContent())){
-            return new ErrorMsg(Error.SMS_SHORTCODE_ERROR, "验证码错误。");
-        }*/
-        // 草稿未完善信息
-        accountInfo.setFlag(9);
-        // 查询二级或者三级推荐人
-        if (null != accountInfo.getFatherId() && accountInfo.getFatherId() > 0) {
-            CAccountInfo gtemplate = new CAccountInfo();
-            gtemplate.setId(accountInfo.getFatherId());
-
-            List<CAccountInfo> infos = accountService.getList(gtemplate, 0, 0);
-            if (infos.size() > 0) {
-                accountInfo.setGrandId(infos.get(0).getFatherId());
-                accountInfo.setGgrandId(infos.get(0).getGgrandId());
-            }
-        }
-        accountInfo.setPassword(MD5Util.MD5Encode(SystemConstant.PREFIX_MD5 + accountInfo.getPassword(), "UTF-8"));
-        accountService.add(accountInfo);
-        return new ErrorMsg(Error.SUCCESS.getValue(), Error.SUCCESS.getReasonPhrase(), accountInfo.getId());
     }
 }
