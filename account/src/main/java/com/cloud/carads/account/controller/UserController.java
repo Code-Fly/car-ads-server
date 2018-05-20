078b309ece31bc7d7ab36709ee2b84326e513ad0
Partys88 <Zqw19880706>
2018-05-20 14:45:07 +0800

    完善车主接口信息

--- account/src/main/java/com/cloud/carads/account/controller/UserController.java
+++ account/src/main/java/com/cloud/carads/account/controller/UserController.java
@@ -6,14 +6,14 @@ package com.cloud.carads.account.controller;
 import com.cloud.carads.account.entity.CAccountInfo;
 import com.cloud.carads.account.entity.CAccountInfoDto;
 import com.cloud.carads.account.entity.CAccountInfoExample;
-import com.cloud.carads.account.service.IUserService;
+import com.cloud.carads.account.entity.SmsLog;
+import com.cloud.carads.account.service.IAccountService;
+import com.cloud.carads.account.service.ISMSService;
 import com.cloud.carads.commons.controller.BaseController;
 import com.cloud.carads.commons.entity.Error;
 import com.cloud.carads.commons.entity.ErrorMsg;
 import com.cloud.carads.commons.utils.MD5Util;
 import com.cloud.carads.constant.SystemConstant;
-import com.cloud.carads.sms.entity.SmsLog;
-import com.cloud.carads.sms.service.SMSService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -32,10 +32,10 @@ import java.util.List;
 @Api(description = "车主管理接口")
 public class UserController extends BaseController {
     @Autowired
-    private IUserService userService;
+    private IAccountService userService;
 
     @Autowired
-    private SMSService smsService;
+    private ISMSService smsService;
 
     @GetMapping(value = "/querybyid", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ApiOperation(value = "根据id获取车主信息")
@@ -50,7 +50,7 @@ public class UserController extends BaseController {
                                       @RequestBody(required = true)CAccountInfoDto accountInfo
                               ) {
         // 校验验证码
-        SmsLog smsLog = smsService.queryLastSMSByPhone(accountInfo.getMobileNo());
+        ErrorMsg smsLog = smsService.queryLastSMSByPhone(accountInfo.getMobileNo());
         //ToDO
        /* if (!accountInfo.getShortCode().equals(smsLog.getContent())){
             return new ErrorMsg(Error.SMS_SHORTCODE_ERROR, "验证码错误。");
