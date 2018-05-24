7f3a9c45e1ba0b523c68e484f411c1ed2372bd69
Partys88 <Zqw19880706>
2018-05-24 21:35:54 +0800

    去掉验证码校验

--- account/src/main/java/com/cloud/carads/account/service/ISMSService.java
+++ account/src/main/java/com/cloud/carads/account/service/ISMSService.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "car-ads-system")
 public interface ISMSService {
     @GetMapping(value = "/smss/latest", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ApiOperation(value = "获取最新短信的车主app信息,返回的字段说明请看c_app_version表")
+    @ApiOperation(value = "获取最新的短信验证码")
     ErrorMsg getAPPVersion(
             @ApiParam(value = "发送手机号码")
             @RequestParam(value = "phoneNo", required = true) String phoneNo
